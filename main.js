d32ca9aa2ec5d007410b27e837f33bff25371a53
Grady Robbins <gradyrobbins13@gmail.com>
2021-10-20 09:07:53 -0500

    the boilerplate threejs code renders now

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-// console.log("hello world ");
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/three@0.133.1/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-const animate = function() {
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-};
-
+let camera, scene, renderer;
+init();
 animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const material = new THREE.MeshNormalMaterial();
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 0.5;
+    controls.maxDistance = 5;
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+}
